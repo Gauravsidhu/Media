ca542a6d09144698c238e95a9f9a647145893531
Gauravsidhu <sidhugaurav69@gmail.com>
2025-02-04 10:49:28 +0530

    changes in the index file

--- index.php
+++ index.php
@@ -1,17 +1,20 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Scroll Animation Example</title>
     <link rel="stylesheet" href="styles.css">
 </head>
+
 <body>
     <div class="content">
         <div class="text-container">
             <div class="text" id="text1">
                 <h2>Text 1</h2>
                 <p>This is the default text.</p>
+                <p>This is the new text</p>
             </div>
             <div class="text" id="text2">
                 <h2>Text 2</h2>
@@ -21,4 +24,5 @@
     </div>
     <script src="script.js"></script>
 </body>
+
 </html>
